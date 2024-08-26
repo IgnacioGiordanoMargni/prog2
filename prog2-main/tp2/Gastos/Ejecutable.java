50a947bd6aca4c28e8d92744ca703c707579ce77
IgnacioGiordanoMargni <131714266+IgnacioGiordanoMargni@users.noreply.github.com>
2024-08-26 15:47:26 -0300

    Delete prog2-main directory

--- prog2-main/tp2/Gastos/Ejecutable.java
+++
@@ -1,9 +0,0 @@
-public class Ejecutable {
-    public static void main(String[] args) {
-        Pais pais= new Pais(5);
-        pais.analizarProvinciasYCiudades();
-        Provincia[] provincia= pais.getProvincias();
-
-
-    }
-}
--- prog2-main/tp2/Series/Ejecutable.java
+++
@@ -1,9 +0,0 @@
-public class Ejecutable {
-    public static void main(String[] args) {
-        
-         Serie serie= new Serie(3, 5);
-         System.out.println(serie.promedioCalificaciones());
-         
-        
-    }
-}
